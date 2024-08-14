3c753951ea4866f8690ee38001f088176ddf8473
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-14 23:45:45 +0530

    bug fix

--- src/Scroll-Indicator/index.jsx
+++ src/Scroll-Indicator/index.jsx
@@ -4,14 +4,13 @@ import { useState } from "react";
 export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState("");
 
-  async function fetchData(getUrl){
-    try{
-        
-    }catch(e){
-        console.log(e);
-        setErrorMessage(e.message)
+  async function fetchData(getUrl) {
+    try {
+    } catch (e) {
+      console.log(e);
+      setErrorMessage(e.message);
     }
   }
 
