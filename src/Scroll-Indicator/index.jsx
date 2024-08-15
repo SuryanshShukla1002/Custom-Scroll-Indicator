acfa5c6ca6779e4d53b2e93ca1f33cba032020dd
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-15 22:01:45 +0530

    funtion created on scroll top to heaight and client

--- src/Scroll-Indicator/index.jsx
+++ src/Scroll-Indicator/index.jsx
@@ -5,16 +5,61 @@ export default function ScrollIndicator({ url }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   async function fetchData(getUrl) {
     try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
     } catch (e) {
       console.log(e);
       setErrorMessage(e.message);
     }
   }
+  const handleScrollPercentage = () => {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+  };
 
   useEffect(() => {
     fetchData(url);
   }, [url]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  console.log(data, loading);
+
+  return (
+    <div>
+      <h1>Custom Scroll Indicator</h1>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
 }
